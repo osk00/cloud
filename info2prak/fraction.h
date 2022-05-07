5bb36b92657b947431a2386125753c7023fb8cb1
Obada Alkhatib <osk.sn00@gmail.com>
2022-05-08 00:02:59 +0200

    uploading the 2 Praktikum

---
+++ info2prak/fraction.h
@@ -0,0 +1,16 @@
+#ifndef FRACTION_H
+#define FRACTION_H
+
+
+class fraction
+{
+    public:
+        fraction();
+        virtual ~fraction();
+
+    protected:
+
+    private:
+};
+
+#endif // FRACTION_H
